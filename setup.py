0775563b9df6d2a79eb6190344bd6194dd4e8368
dcherian <deepak@cherian.net>
2020-10-06 21:20:57 -0600

    Remove versioneer, update setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
+#!/usr/bin/env python
 from setuptools import setup
 
 setup(
-    name="xfilter",
-    version="0.1",
-    description="xarray-aware filtering",
-    url="http://github.com/dcherian/xfilter",
-    author="Deepak Cherian",
-    author_email="deepak@cherian.net",
-    license="GPL3",
-    packages=["xfilter"],
-    zip_safe=False,
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="Xarray-aware filtering. ",
+    url="https://github.com/dcherian/xfilter",
 )
